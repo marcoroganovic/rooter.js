e34dd3aae5bc9fa93d7cc57662bccbe551c6cc42
Marco Roganovic <marcoroganovic@gmail.com>
2017-02-12 15:33:25 +0100

    completely rewrite prototype-based version

--- rooter.js
+++ rooter.js
@@ -1,18 +1,19 @@
-(function (lib, global) {
-
+(function(lib, global) {
+  
   if(typeof module === "object" && typeof module.exports === "object") {
-    module.exports = lib();
+    module.exports = lib;
   } else if(typeof define === "function" && define.amd) {
     return define([], lib);
   } else {
-    global.rooter = lib();
+    global.rooter = lib;
   }
 
-})(function() {
+})(function(id) {
 
-  "use strict";
-
-  // Helpers
+  /*******************
+   * Helper functions
+  ********************/
+  
   function noop() {}
 
   
@@ -21,8 +22,13 @@
   }
 
   
-  function isObject(arg) {
-    return typeof arg === "object";
+  function isNumber(arg) {
+    return typeof arg === "number";
+  }
+
+  
+  function isFunction(arg) {
+    return typeof arg === "function";
   }
 
   
@@ -31,15 +37,10 @@
   }
 
   
-  function isFunc(arg) {
-    return typeof arg === "function";
-  }
-
-  
   function typeError(expected, actual) {
     throw new TypeError("Expected " + expected + ", got " + actual);
   }
-  
+
   
   function isRegexPath(path) {
     return /\/:[^\/]+/g.test(path);
@@ -63,7 +64,6 @@
     return params;
   }
 
-  
   function prefixPath(path, prefix) {
     if(prefix) {
       if(prefix.indexOf("/") === -1) {
@@ -72,235 +72,265 @@
         path = prefix + (path === "/" ? "" : path);
       }
     }
-    
+
     return path;
   }
 
-
-  function Router(root) {
-    this.routes = [];
-    this.prefix = null;
-    this.$root = document.getElementById(root);
-    this.activeRoute = window.location.pathname;
-    this.transitionTime = 400;
-    this.notFoundHandler = noop
-  }
-
-
-  Router.prototype = {
-
-
-    namespace: function(name) {
-      this.prefix = name;
-      return this;
-    },
-
-
-    easeIn: function() {
-      this.$root.style.opacity = 1;
-      this.$root.style.transition = "opacity .5s ease-in";
-    },
-
-
-    easeOut: function() {
-      this.$root.style.opacity = 0;
-    },
-
-    
-    getTransitionTime: function() {
-      return this.transitionTime;
-    },
-
-    
-    setTransitionTime: function(amount) {
-      this.transitionTime = amount;
-    },
-
-
-    transitionView: function(route) {
-      this.easeOut();
-        setTimeout(function() {
-          route.handler(route.params ?
-            extractParams(route) :
-            null
-          );
-          this.easeIn();
-        }.bind(this), this.transitonTime);
-    },
-
-
-    changeView: function(route) {
-      // route is function on 404 page
-      if(isFunc(route)) {
-        route();
-        return;
-      }
-
-      this.transitionView(route);
-    },
-
-
-    addRegexRoute(path, cb, mw) {
-      var placeholder = /\/:[^\/]+/g;
-      var params = path.match(placeholder);
-      path = new RegExp("^" + path.replace(placeholder, '/([^/]+)') + "$");
-
-      this.routes.push({
-        path: path,
-        handler: cb,
-        isRegex: true,
-        params: params,
-        middleware: mw || null
-      });
-    },
-
-
-    addRouteMiddleware: function(path, mw, cb) {
-      if(!isString(path)) typeError("string", typeof path);
-      if(!isArray(mw)) typeError("array", typeof mw);
-      if(!isFunc(cb)) typeError("function", typeof cb);
-
-      if(isRegexPath(path)) {
-        this.addRegexRoute(path, cb, mw);
-      } else {
-        this.routes.push({ path: path, middleware: mw, handler: cb });
-      }
-    },
-
-
-    addRoute: function(path, handler) {
-      if(!isString(path)) typeError("string", typeof path);
-      if(!isFunc(handler)) typeError("function", typeof handler);
-
-      if(isRegexPath(path)) {
-        this.addRegexRoute(path, handler);
-      } else {
-        this.routes.push({ path: path, handler: handler });
-      }
-    },
-
-
-    when: function(path, middleware, handler) {
-      // constructs path with prefix if set by namespace method
-      path = prefixPath(path, this.prefix);
-      
-      switch(arguments.length) {
-        case 3:
-          this.addRouteMiddleware(path, middleware, handler);
-          break;
-        case 2:
-          handler = middleware;
-          this.addRoute(path, handler);
-          break;
-      }
-      return this;
-    },
-
-
-    checkForRouteChange: function(path) {
-      if(this.activePath !== path) {
-        this.activePath = path;
-        this.findRoute(path);
-      }
-    },
-
-
-    executeMiddleware: function(arr) {
-      arr.forEach(function(mw) {
-        if(isFunc(mw)) mw();
-      });
-    },
-
-
-    returnMatch: function(path) {
-      return this.routes.find(function(route) {
-        return (route.isRegex && path.match(route.path)) ||
-                route.path === path ? route : false;
-      });
-    },
-
-
-    findRoute: function(path) {
-      var match = this.returnMatch(path);
-
-      if(match) {
-        if(match.middleware) this.executeMiddleware(match.middleware);
-        this.changeView(match);
-      } else {
-        this.changeView(this.notFoundHandler);
-      }
-    },
-
-    
-    setupClickListener: function() {
-      var self = this;
-      document.addEventListener("click", function(e) {
-        var el = e.target;
-        if(el.nodeName.toLowerCase() === "a") {
-          if(el.href.indexOf(window.location.host) !== -1) {
-            e.preventDefault();
-            history.pushState(el.pathname, null, el.pathname);
-            self.checkForRouteChange(el.pathname);
-          }
-        }
-      }, false);
-    },
-
-    
-    setupPopstateListener: function() {
-      var self = this;
-      window.addEventListener("popstate", function(e) {
-        self.findRoute(e.state);
-      });
-    },
-
-    
-    start: function() {
-      this.setupClickListener();
-      this.setupPopstateListener();
-      this.findRoute(window.location.pathname);
-    },
-
-
-    notFound: function(handler) {
-      this.notFoundHandler = handler;
-      return this;
-    },
-
-    
-    goTo: function(path) {
-      history.pushState(path, null, path);
-      this.findRoute(path);
-    },
-
-    
-    redirect: function(path) {
-      this.goTo(path);
-    },
-
-    
-    remove: function(path) {
-      this.routes = this.routes.filter(function(route) {
-        return (route.isRegex && path.match(route.path)) ||
-               (route.path === path) ? false: true;
-      });
-    },
-
-    
-    flush: function() {
-      this.route = [];
-      this.activeRoute = window.location.pathname,
-      this.prefix = null;
-      this.notFoundHandler = noop;
-    }
-
-  };
-
-
-  return function(id) { 
-    if(!(this instanceof Router)) {
-      return new Router(id);
-    }
-  }
   
-}, window);
+  
+  
+  
+  /********************
+   * Object properties
+  *********************/
+  var routes = [];
+  var prefix = null;
+  var activePath = window.location.pathname;
+  var $root = document.getElementById(id) || null;
+  var transitionTime = 400;
+  var notFoundHandler = noop;
+  
+  
+  
+  
+  
+  /*******************
+   * Private methods
+  ********************/
+
+  function easeIn() {
+    $root.style.opacity = 1;
+    $root.style.transition = "opacity .5s ease-in";
+  }
+
+  
+  function easeOut() {
+    $root.style.opacity = 0;
+  }
+
+  
+  function getTransitionTime() {
+    return transitionTime;
+  }
+
+  
+  function setTransitionTime(amount) {
+    transitionTime = amount;
+  }
+
+  
+  function transitionView(route) {
+    easeOut();
+    setTimeout(function() {
+      route.handler(route.params ? extractParams(route) : null);
+      easeIn();
+    }, transitionTime);
+  }
+
+  
+  function changeView(route) {
+    // handle not found page
+    if(isFunction(route)) {
+      route(); 
+      return;
+    }
+
+    if($root) {
+      transitionView(route);
+    } else {
+      route.handler(route.params ? extractParams(route) : null);
+    }
+  }
+
+  
+  function addRegexRoute(path, cb, mw) {
+    var placeholder = /\/:[^\/]+/g;
+    var params = path.match(placeholder);
+    path = new RegExp("^" + path.replace(placeholder, '/([^/]+)') + "$");
+
+    routes.push({
+      path: path,
+      handler: cb,
+      isRegex: true,
+      params: params,
+      middleware: mw || null
+    });
+  }
+
+  
+  function addRouteMiddleware(path, mw, cb) {
+    if(!isString(path)) typeError("string", typeof path);
+    if(!isArray(mw)) typeError("array", typeof mw);
+    if(!isFunction(cb)) typeError("function", typeof cb);
+
+    if(isRegexPath(path)) {
+      addRegexRoute(path, cb, mw);
+    } else {
+      routes.push({ path: path, middleware: mw, handler: cb });
+    }
+  }
+
+
+  function addRoute(path, handler) {
+    if(!isString(path)) typeError("string", typeof path);
+    if(!isFunction(handler)) typeError("function", typeof handler);
+
+    if(isRegexPath(path)) {
+      addRegexRoute(path, handler);
+    } else {
+      routes.push({ path: path, handler: handler });
+    }
+  }
+
+  
+  function returnMatch(path) {
+    return routes.find(function(route) {
+      return (route.isRegex && path.match(route.path)) ||
+              route.path === path ? route : false;
+    });
+  }
+
+  
+  function executeMiddleware(arr) {
+    arr.forEach(function(mw) {
+      if(isFunction(mw)) mw();
+    });
+  }
+
+  
+  function findRoute(path) {
+    var match = returnMatch(path);
+    
+    if(match) {
+      if(match.middleware) executeMiddleware(match.middleware);
+      changeView(match);
+    } else {
+      changeView(notFoundHandler);
+    }
+  }
+
+
+  function checkForRouteChange(path) {
+    if(activePath !== path) {
+      activePath = path;
+      findRoute(path);
+    }
+  }
+
+
+  function setupClickListener() {
+    document.addEventListener("click", function(e) {
+      var el = e.target;
+      if(el.nodeName.toLowerCase() === "a") {
+        if(el.href.indexOf(window.location.host) !== -1) {
+          e.preventDefault();
+          history.pushState(el.pathname, null, el.pathname);
+          checkForRouteChange(el.pathname);
+        }
+      }
+    }, false);
+  }
+
+
+  function setupPopstateListener() {
+    window.addEventListener("popstate", function(e) {
+      findRoute(e.state);
+    });
+  }
+
+  
+  
+  
+  
+  /*****************
+  * Public methods
+  ******************/
+
+  function when(path, middleware, handler) {
+    // construct path with prefix if is set by namespace method
+    path = prefixPath(path, prefix);
+
+    if(arguments.length >= 3) {
+      addRouteMiddleware(path, middleware, handler);
+    } else {
+      handler = middleware;
+      addRoute(path, handler);
+    }
+
+    return this;
+  }
+
+
+  function getRoutes() {
+    return routes;
+  }
+
+
+  function namespace(name) {
+    prefix = name;
+    return this;
+  }
+
+  
+  function start() {
+    setupClickListener();
+    setupPopstateListener();
+    findRoute(window.location.pathname);
+  }
+
+ 
+  function goTo(path) {
+    history.pushState(path, null, path);
+    findRoute(path);
+  }
+
+
+  function redirect(path) {
+    this.goTo(path);
+  }
+
+
+  function notFound(handler) {
+    notFoundHandler = handler;
+    return this;
+  }
+
+  
+  function remove(path) {
+    routes = routes.filter(function(route) {
+      return (route.isRegex && path.match(route.path)) ||
+             (route.path === path) ? false : true;
+    });
+  }
+
+
+  function flush() {
+    routes = [];
+    activePath = window.location.pathname;
+    prefix = null;
+    notFoundHandler = noop;
+  }
+
+
+  
+  
+  
+  /********************
+   * Export Public API
+  *********************/
+
+  return {
+    when: when,
+    notFound: notFound,
+    start: start,
+    namespace: namespace,
+    getRoutes: getRoutes,
+    getTransitionTime: getTransitionTime,
+    setTransitionTime: setTransitionTime,
+    goTo: goTo,
+    redirect: redirect,
+    remove: remove,
+    flush: flush
+  }
+
+}, this);
